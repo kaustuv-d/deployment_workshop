c5b51b477faae4feec2764c34dc480904bf051b7
Kaustuv Devmishra <kaustuv2003@gmail.com>
2023-10-04 12:53:45 +0530

    first commit

---
+++ basic.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+
+# app = FastAPI()
+
+# @app.get('/')
+# def root_api():
+#     return{"message" : "Winter is Coming, Jon"}
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, port=8000, host="127.0.0.1")
---
+++ main_01.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import uvicorn
+from gradio_client import Client
+
+
+
+
+
+
+# app = FastAPI()
+
+
+
+
+# @app.get("/")
+# async def root():
+#   return "Working"
+
+# @app.post("/generate")
+# async def generate_image(prompt: str):
+#     img_path = gen_image(prompt)
+#     return FileResponse(img_path)
+
+# if __name__ == "__main__":
+#   uvicorn.run(app, port=8000, host="127.0.0.1")
+
+
