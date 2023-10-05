1df0695b22f0421fe9773bc07f742b8479068348
Kaustuv Devmishra <117767351+kaustuv-d@users.noreply.github.com>
2023-10-05 09:21:15 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,13 +1,13 @@
+#pip install fastapi
+#pip install uvicorn
 from fastapi import FastAPI
 import uvicorn
 
+app = FastAPI()
 
+@app.get('/')
+def root_api():
+    return{"message": "AI Winter is Coming"}
 
-# app = FastAPI()
-
-# @app.get('/')
-# def root_api():
-#     return{"message" : "Winter is Coming, Jon"}
-
-# if __name__ == "__main__":
-#     uvicorn.run(app, port=8000, host="127.0.0.1")
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="127.0.0.1")
